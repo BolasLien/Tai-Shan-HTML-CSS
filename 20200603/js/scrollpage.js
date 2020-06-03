9f222437564fdd1c927c1df1fb96969cf2cfd1f4
BolasLien <ltzuen@gmai.com>
2020-06-03 16:28:18 +0800

    上課檔案

---
+++ 20200603/js/scrollpage.js
@@ -0,0 +1,10 @@
+$('a[href^="#"]').on('click', function(event) {
+  let $target = $(this.getAttribute('href'));
+  if ($target.length) {
+    event.preventDefault();
+    $('html,body').stop().animate({
+        scrollTop: $target.offset().top
+      }, 1000,
+      'easeInOutBack')
+  }
+})
